b170d322db5d58d2f658bafe8297c2c519b79e1b
marco-soria <marco.soriaa7@gmail.com>
2025-04-25 20:31:20 -0500

    changed state

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 //import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './user/user.component';
 
 @Component({
   selector: 'app-root',
   imports: [
     //RouterOutlet,
     HeaderComponent,
+    UserComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
