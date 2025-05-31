cdff86d7f17016cc086daa5dad7b9151bad007f0
marco-soria <marco.soriaa7@gmail.com>
2025-05-31 10:15:44 -0500

    exercisept1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from './dummy-users';
+import { TasksComponent } from './tasks/tasks.component';
 
 @Component({
   selector: 'app-root',
@@ -10,6 +11,7 @@ import { DUMMY_USERS } from './dummy-users';
     //RouterOutlet,
     HeaderComponent,
     UserComponent,
+    TasksComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
@@ -17,7 +19,13 @@ import { DUMMY_USERS } from './dummy-users';
 export class AppComponent {
   users = DUMMY_USERS;
 
+  selectedUserId = 'u1';
+
+  get selectedUser() {
+    return this.users.find((user) => user.id === this.selectedUserId)!;
+  }
+
   onSelectUser(id: string) {
-    console.log('Selected user with id ' + id);
+    this.selectedUserId = id;
   }
 }
