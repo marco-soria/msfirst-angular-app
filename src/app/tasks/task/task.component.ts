5c27ec81c9527b8475e6f4cf3a7df47d8e53ddd3
marco-soria <marco.soriaa7@gmail.com>
2025-06-02 20:44:44 -0500

    service remove

--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,8 +1,9 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
 import { DatePipe } from '@angular/common';
 
 import { type Task } from '../task/task.model';
 import { CardComponent } from '../../shared/card/card.component';
+import { TasksService } from '../tasks.service';
 @Component({
   selector: 'app-task',
   imports: [CardComponent, DatePipe],
@@ -11,9 +12,9 @@ import { CardComponent } from '../../shared/card/card.component';
 })
 export class TaskComponent {
   @Input({ required: true }) task!: Task;
-  @Output() complete = new EventEmitter<string>();
+  private tasksService = inject(TasksService);
 
   onCompleteTask() {
-    this.complete.emit(this.task.id);
+    this.tasksService.removeTask(this.task.id);
   }
 }
